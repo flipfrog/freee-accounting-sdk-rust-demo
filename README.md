80ba70cfb6871c53f65a837abcc889acb60624f6
flipfrog <>
2022-06-18 19:56:18 +0900

    add api transmission process.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# freee-accounting-sdk-rust-demo
- freee-accounting-sdk-rust-demo
+# freee-accounting-sdk-rustリポジトリを使ったデモプログラム
+
+このリポジトリのコードは、Raspberry Piでコンパイルします。
+
+Windowsや、MacOS上では、コンパイルできないので注意してください。
+
+## プログラムの内容
+- Raspberry PiのGPIOに結線されたスイッチが押されたら、freee会計のPublic API経由で、経費申請を下書き作成します。
+- スイッチは、GPIOの21番とGNDに接続し、スイッチ押下時に回路が短絡するようにします。
+- Raspberry Pi内部のプルアップ回路を使用するので、回路上にプルアップ抵抗などを使う必要はありません。
+
+このリポジトリと同じ階層に、[freee-accounting-sdk-rust](https://github.com/flipfrog/freee-accounting-sdk-rust)リポジトリの内容を配置します。
+
+## 実行のための準備
+- freeeのアプリストアで、新規にアプリを登録します。
+- アプリ設定のコールバックURLに、Raspberry Piのホスト名を使い、`http://<host>:8080/auth_callback` を設定し、保存します。
+- cloneした、freee-accounting-sdk-rustリポジトリの内容を変更します。
+    - 下記に書かれているURLを、先に設定したコールバックURLに修正します。
+    - https://github.com/flipfrog/freee-accounting-sdk-rust/blob/72e1f11fa3d5c19b92cf8aaf097152d8fc6fd416/examples/web/src/main.rs#L161 
+    - 下記に書かれているホスト名を、`0.0.0.0` に修正します。
+    - https://github.com/flipfrog/freee-accounting-sdk-rust/blob/72e1f11fa3d5c19b92cf8aaf097152d8fc6fd416/examples/web/src/main.rs#L192
+- Raspberry Pi 上で、freee-accounting-sdk-rustのwebサンプルを実行して、アプリを認証します。認証した時点で、sqliteデータベースにfreee Public APIのアクセストークンが格納されます。
+  認証時に、経費申請を作成したい事業所のIDをメモします。
+- 環境変数 `RUST_API_EXAMPLE_COMPANY_ID` に、上記の経費申請を作成したいfreeeの事業所IDを設定します。
+
+## コンパイルと実行方法
+
+- `cargo run` を実行します。
+
+## 経費申請の作成方法
+
+- Raspberry Piにつないた、スイッチを押します。
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::env;
 use r2d2_sqlite::{self, SqliteConnectionManager};
 type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
 use rusqlite::{params};
 use openapi_sdk::apis::configuration::Configuration;
 use openapi_sdk::apis::companies_api;
+use openapi_sdk::apis::expense_applications_api::create_expense_application;
+use openapi_sdk::models::ExpenseApplicationCreateParams;
+use openapi_sdk::models::ExpenseApplicationCreateParamsExpenseApplicationLines;
+use rppal::gpio::{Gpio, Trigger};
+
+const GPIO_NO: u8 = 21;
 
 #[tokio::main]
 async fn main() {
@@ -21,6 +28,9 @@ async fn main() {
         .expect("DBからのアクセストークンの取得に失敗しました");
     let token = rows.reduce(|_a, t| t).unwrap().unwrap();
 
+    // 環境変数から、事業所IDを取得する
+    let company_id = env::var("RUST_API_EXAMPLE_COMPANY_ID").expect("事業所IDの取得に失敗しました");
+
     // api configuration
     let config = Configuration {
         base_path: "https://api.freee.co.jp".to_string(),
@@ -32,9 +42,8 @@ async fn main() {
         api_key: None
     };
 
-    // APIで事業所の一覧を取得する
-    let companies = companies_api::get_companies(&config).await
-        .expect("事業所一覧の取得に失敗しました");
+    // 事業所の一覧を取得する
+    let companies = companies_api::get_companies(&config).await.expect("事業所一覧の取得に失敗しました");
 
     for company in &companies.companies {
         println!("- company.id: {}, company.display_name: {}",
@@ -43,5 +52,48 @@ async fn main() {
         );
     }
 
-    // TODO: ループしてGPIOのポートがhighになったら、取引を登録する
+    // 経費精算申請のパラメータを作成する
+    let line = ExpenseApplicationCreateParamsExpenseApplicationLines {
+        transaction_date: Some("2022-06-10".to_string()),
+        amount: Some(100),
+        description: Some("コーヒー代".to_string()),
+        receipt_id: None,
+        expense_application_line_template_id: None
+    };
+    let params = ExpenseApplicationCreateParams {
+        company_id: company_id.parse().unwrap(),
+        title: "会議費".to_string(),
+        expense_application_lines: vec!(line),
+        approval_flow_route_id: None,
+        approver_id: None,
+        description: None,
+        draft: Some(true),
+        issue_date: Some("2022-06-10".to_string()),
+        parent_id: None,
+        section_id: None,
+        segment_1_tag_id: None,
+        segment_2_tag_id: None,
+        segment_3_tag_id: None,
+        tag_ids: None,
+    };
+
+    let gpio = Gpio::new().expect("Error in getting Gpio instance.");
+    let gpio_port = gpio.get(GPIO_NO).expect("Error in getting GPIO Port.");
+    let mut switch = gpio_port.into_input_pullup();
+    switch.set_interrupt(Trigger::FallingEdge).expect("Error in setting interrupt.");
+
+    // ループしてGPIOポート信号の立ち下がりで、取引を登録する
+    loop {
+        let level = switch.poll_interrupt(true, None);
+        match level {
+            Ok(_) => {
+                create_expense_application(&config, Some(params.clone())).await.expect("経費申請の作成に失敗しました");
+                println!("経費申請を作成しましました。");
+            },
+            Err(e) => {
+                println!("Error: {:?}", e);
+                break;
+            }
+        }
+    }
 }
